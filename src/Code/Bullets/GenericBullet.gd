9c0c3e798358b533fae2085955c90af44c6af30b
AlexM <tempthing0@gmail.com>
2021-08-23 10:38:36 -0700

    Setup Generic items

--- src/Code/Bullets/GenericBullet.gd
+++ src/Code/Bullets/GenericBullet.gd
@@ -3,9 +3,11 @@ extends KinematicBody2D
 
 export var speed = 300
 export var damage = 1
+export var rotation_speed = 6
 
-func _physics_process(_delta):
+func _physics_process(delta):
 	var _vel = move_and_slide(Vector2(1,0).rotated(rotation) * speed)
+	$Sprite.rotation +=  rotation_speed * delta
 	
 	for i in get_slide_count():
 		var col: KinematicCollision2D = get_slide_collision(i)
