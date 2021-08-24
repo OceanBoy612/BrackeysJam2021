a2bb001db8280564f90a7c811a57acd9692eed3a
Yonatan Attali <yonatan.attali@gmail.com>
2021-08-24 15:42:52 -0400

    added properties to the bullets

--- src/Code/Bullets/GenericBullet.gd
+++ src/Code/Bullets/GenericBullet.gd
@@ -4,8 +4,11 @@ extends KinematicBody2D
 signal destroyed
 
 
-export var speed = 300
+export var speed: float = 300
 export var damage = 1
+#export(int, 25, 1000, 25) var distance = 200 # distance in pixels the bullet will travel
+export var time_alive: float = 1.0
+export var shot_cone = 0.0 # 0 means perfectly accurate, 30 means bullets can go anywhere in a 30 degree cone
 export var rotate = true
 
 
@@ -13,16 +16,31 @@ onready var fragment_tscn = preload("res://Code/Fragmentation/Fragment.tscn")
 
 
 var rotation_speed = 0
+var distance_traveled = 0
+var decay_rate: float
+var max_speed: float
 
 
 func _ready():
 	if rotate:
 		rotation_speed = rand_range(4, 12)
+	rotation_degrees += rand_range(-shot_cone/2, shot_cone/2)
+	
+	$Tween.interpolate_property(self, "speed", speed, 0, time_alive, Tween.TRANS_SINE, Tween.EASE_IN)
+	$Tween.start()
+	
 
 
 func _physics_process(delta):
+#	distance_traveled += speed * delta
+#	print(speed, "   ", distance_traveled)
+	
 	var _vel = move_and_slide(Vector2(1,0).rotated(rotation) * speed)
 	$Sprite.rotation +=  rotation_speed * delta
+	
+	if speed == 0:
+		die()
+		return
 	
 	for i in get_slide_count():
 		var col: KinematicCollision2D = get_slide_collision(i)
@@ -45,3 +63,4 @@ func die():
 	get_parent().add_child(fragment)
 	
 	queue_free() 
+
