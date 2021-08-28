c280de5ee439bf1517b18e8c171d5a0f6c54340b
Yonatan Attali <yonatan.attali@gmail.com>
2021-08-28 17:16:58 -0400

    fixed items being null on pickup

--- src/Code/Managers/Globals.gd
+++ src/Code/Managers/Globals.gd
@@ -10,6 +10,11 @@ onready var items = {
 	"chair": preload("res://Code/Bullets/ChairBullet.tscn"),
 	"bomb": preload("res://Code/Bullets/BombBullet.tscn"),
 	"hedgehog": preload("res://Code/Bullets/HedgeHogBullet.tscn"),
+	"teddy": preload("res://Code/Bullets/TeddyBullet.tscn"),
+	"teapot": preload("res://Code/Bullets/TeapotBullet.tscn"),
+	"quill": preload("res://Code/Bullets/QuillBullet.tscn"),
+	"pumpkin": preload("res://Code/Bullets/PumpkinBullet.tscn"),
+	"flower": preload("res://Code/Bullets/FlowerBullet.tscn"),
 }
 
 func _ready():
