96dbccb148353b6b39f2bd5c4499af5a36b0d4dd
AlexM <tempthing0@gmail.com>
2021-08-28 22:41:57 -0700

    Added stuff

--- src/Code/Spawner/LootBox.gd
+++ src/Code/Spawner/LootBox.gd
@@ -16,33 +16,40 @@ var lootcount = 0
 
 func _ready():
 	get_node("/root/World").connect("room_cleared", self, "_get_loot")
+	yield(get_tree().create_timer(0.2), "timeout")
+	if lootroom == -1:
+		drop_loot()
+		print("TEST")
 
 
 func _get_loot():
-	if lootcount < lootroom:
+	if lootcount == lootroom:
 		lootcount += 1
-	elif lootcount == lootroom:
+		drop_loot()
+	else:
 		lootcount += 1
-		var lootnum = int(rand_range(1, 11))
-		var entity_tscn = item1
-		match lootnum:
-			2:
-				entity_tscn = item2
-			3:
-				entity_tscn = item3
-			4:
-				entity_tscn = item4
-			5:
-				entity_tscn = item5
-			6:
-				entity_tscn = item6
-			7:
-				entity_tscn = item7
-			8:
-				entity_tscn = item8
-			9:
-				entity_tscn = item9
-		var entity = entity_tscn.instance()
-		entity.global_position = global_position
-		get_node("/root/World").add_child(entity)
-		print("Loot spawned")
+
+func drop_loot():
+	var lootnum = int(rand_range(1, 11))
+	var entity_tscn = item1
+	match lootnum:
+		2:
+			entity_tscn = item2
+		3:
+			entity_tscn = item3
+		4:
+			entity_tscn = item4
+		5:
+			entity_tscn = item5
+		6:
+			entity_tscn = item6
+		7:
+			entity_tscn = item7
+		8:
+			entity_tscn = item8
+		9:
+			entity_tscn = item9
+	var entity = entity_tscn.instance()
+	entity.global_position = global_position
+	get_node("/root/World").add_child(entity)
+	print("Loot spawned")
